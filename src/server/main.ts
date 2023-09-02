6761bc5d084efe85bd52640334a0ad8fca09899e
moonrailgun <moonrailgun@gmail.com>
2023-09-02 20:13:50 +0800

    feat: add prisma

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import ViteExpress from 'vite-express';
 
---
+++ src/server/model/user.ts
@@ -0,0 +1,46 @@
+import { prisma } from './_client';
+import bcryptjs from 'bcryptjs';
+
+async function hashPassword(password: string) {
+  return await bcryptjs.hash(password, 10);
+}
+
+/**
+ * Create User
+ */
+export async function createAdminUser(username: string, password: string) {
+  const count = await prisma.user.count();
+
+  if (count > 0) {
+    throw new Error('Create Admin User Just Only allow in non people exist');
+  }
+
+  await prisma.user.create({
+    data: {
+      username,
+      password: await hashPassword(password),
+      role: 'admin',
+    },
+  });
+}
+
+export async function createUser(username: string, password: string) {
+  await prisma.user.create({
+    data: {
+      username,
+      password: await hashPassword(password),
+      role: 'normal',
+    },
+  });
+}
+
+export async function authUser(username: string, password: string) {
+  const user = await prisma.user.findFirstOrThrow({
+    where: {
+      username,
+      password: await hashPassword(password),
+    },
+  });
+
+  return user;
+}
