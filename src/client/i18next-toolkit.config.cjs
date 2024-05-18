2e93dd882cffea829136e531163d2c13ce834ff5
DevMirza <pzhafeez@gmail.com>
2024-05-18 14:36:55 +0000

    Revert "add ur local"
    This reverts commit bd1d3c4849bde1300a88ad790d4f026535dd46e9.

--- src/client/i18next-toolkit.config.cjs
+++ src/client/i18next-toolkit.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('@i18next-toolkit/cli').I18nextToolkitConfig} */
 const config = {
-  locales: ['en', 'zh', 'jp', 'fr', 'de', 'pl', 'pt', 'ru', 'ur'],
+  locales: ['en', 'zh', 'jp', 'fr', 'de', 'pl', 'pt', 'ru'],
   verbose: true,
   namespaces: ['translation'],
   translator: {
