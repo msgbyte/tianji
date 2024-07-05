865e56f40e7351d21c995f7a9c0fafbcc7b75993
moonrailgun <moonrailgun@gmail.com>
2024-07-05 23:28:28 +0800

    chore: fix ci problem

--- src/client/components/feed/FeedEventItem.tsx
+++ src/client/components/feed/FeedEventItem.tsx
@@ -7,7 +7,8 @@ import { MarkdownViewer } from '../MarkdownEditor';
 import { FeedIcon } from './FeedIcon';
 import { cn } from '@/utils/style';
 
-type FeedEventItemType = AppRouterOutput['feed']['events'][number];
+type FeedEventItemType =
+  AppRouterOutput['feed']['fetchEventsByCursor']['items'][number];
 
 export const FeedEventItem: React.FC<{
   className?: string;
