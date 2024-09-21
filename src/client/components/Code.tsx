fb75a8b6545a507c81acafa9cb526afccd39cd35
moonrailgun <moonrailgun@gmail.com>
2024-09-22 00:13:57 +0800

    refactor: fix light mode color issues

--- src/client/components/Code.tsx
+++ src/client/components/Code.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Code: React.FC<React.PropsWithChildren> = React.memo((props) => {
   return (
-    <span className="rounded-sm border border-zinc-800 bg-zinc-900 px-1 py-0.5">
+    <span className="rounded-sm border border-zinc-200 bg-zinc-100 px-1 py-0.5 dark:border-zinc-800 dark:bg-zinc-900">
       {props.children}
     </span>
   );
