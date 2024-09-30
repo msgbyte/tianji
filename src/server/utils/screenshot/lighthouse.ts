13227416c05e2eae5a1a99e5d5f3396679e83d89
moonrailgun <moonrailgun@gmail.com>
2024-09-30 19:54:24 +0800

    chore: upgrade puppeteer usage to fit new version

--- src/server/utils/screenshot/lighthouse.ts
+++ src/server/utils/screenshot/lighthouse.ts
@@ -1,13 +1,18 @@
 import puppeteer from 'puppeteer';
 import lighthouse, { Result, generateReport } from 'lighthouse';
+import { logger } from '../logger.js';
+import { env } from '../env.js';
 
 export async function generateLighthouse(url: string): Promise<Result> {
+  logger.info('[Lighthouse] Generating Lighthouse report...', { url });
+
   // Use Puppeteer to launch headless Chrome
   // - Omit `--enable-automation` (See https://github.com/GoogleChrome/lighthouse/issues/12988)
   // - Don't use 800x600 default viewport
   const browser = await puppeteer.launch({
     // Set to false if you want to see the script in action.
-    headless: 'new',
+    executablePath: env.puppeteerExecutablePath,
+    headless: true,
     args: ['--no-sandbox', '--single-process', '--disable-dev-shm-usage'],
     defaultViewport: null,
     ignoreDefaultArgs: ['--enable-automation'],
