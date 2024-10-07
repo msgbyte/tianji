384224cb624030522057df22527312957665d8e9
moonrailgun <moonrailgun@gmail.com>
2024-10-07 17:16:11 +0800

    docs: add sitemap to improve SEO

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -57,6 +57,18 @@ const config: Config = {
             require.resolve('./src/css/custom.css'),
           ],
         },
+        sitemap: {
+          lastmod: 'date',
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+          createSitemapItems: async (params) => {
+            const { defaultCreateSitemapItems, ...rest } = params;
+            const items = await defaultCreateSitemapItems(rest);
+            return items.filter((item) => !item.url.includes('/page/'));
+          },
+        },
       },
     ],
   ],
