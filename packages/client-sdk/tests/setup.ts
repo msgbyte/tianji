72458fe301267d5e9bcfd2b3903907439bd4d143
moonrailgun <moonrailgun@gmail.com>
2024-11-27 02:39:39 +0800

    chore: upgrade package version to reduce vulnerabilities

--- packages/client-sdk/tests/setup.ts
+++ packages/client-sdk/tests/setup.ts
@@ -10,11 +10,8 @@ export const restHandlers = [
 
 const server = setupServer(...restHandlers);
 
-// 在所有测试之前启动服务器
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 
-// 所有测试后关闭服务器
 afterAll(() => server.close());
 
-// 每次测试后重置处理程序 `对测试隔离很重要`
 afterEach(() => server.resetHandlers());
