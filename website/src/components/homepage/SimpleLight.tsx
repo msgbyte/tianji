99db3d02e370b9d252919d216ad0e1b36540d199
moonrailgun <moonrailgun@gmail.com>
2024-11-30 02:36:46 +0800

    refactor: update code style

--- website/src/components/homepage/SimpleLight.tsx
+++ website/src/components/homepage/SimpleLight.tsx
@@ -6,8 +6,7 @@ export const HomepageSimpleLight: React.FC<{
   colorSet: 'blue' | 'cyan';
 }> = React.memo((props) => {
   const background = {
-    cyan:
-      'radial-gradient(144.11% 103.06% at 50.00% 50.00%, rgba(27, 199, 237, 0.60) 0%, rgba(46, 180, 255, 0.06) 45.67%)',
+    cyan: 'radial-gradient(144.11% 103.06% at 50.00% 50.00%, rgba(27, 199, 237, 0.60) 0%, rgba(46, 180, 255, 0.06) 45.67%)',
     blue: 'radial-gradient(144.11% 103.06% at 50.00% 50.00%, rgba(27, 115, 237, 0.60) 0%, rgba(194, 46, 255, 0.06) 45.67%)',
   }[props.colorSet];
 
@@ -20,7 +19,7 @@ export const HomepageSimpleLight: React.FC<{
       style={{
         background,
       }}
-    ></div>
+    />
   );
 });
 HomepageSimpleLight.displayName = 'HomepageSimpleLight';
