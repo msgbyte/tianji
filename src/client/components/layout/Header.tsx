d688a787427d1fd6d349a7576468021c8a795d13
moonrailgun <moonrailgun@gmail.com>
2024-12-13 21:34:57 +0800

    feat: add paused tip

--- src/client/components/layout/Header.tsx
+++ src/client/components/layout/Header.tsx
@@ -1,12 +1,18 @@
 import { useCurrentWorkspaceSafe } from '@/store/user';
+import { useTranslation } from '@i18next-toolkit/react';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
 export const LayoutHeader: React.FC = React.memo(() => {
+  const { t } = useTranslation();
   const currentWorkspace = useCurrentWorkspaceSafe();
   let title = 'Tianji - Insight into everything';
   if (currentWorkspace) {
     title = currentWorkspace.name + ' | ' + title;
+
+    if (currentWorkspace.paused === true) {
+      title = t('Paused') + ' | ' + title;
+    }
   }
 
   return (
