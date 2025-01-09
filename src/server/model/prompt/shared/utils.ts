eec23ec506dc5cb1498a91c81090f4ebcc2dfb18
moonrailgun <moonrailgun@gmail.com>
2025-01-10 00:39:50 +0800

    feat: add aiToolsSelection which can easy to pick tools

---
+++ src/server/model/prompt/shared/utils.ts
@@ -0,0 +1,49 @@
+import { openaiClient } from '../../openai.js';
+import { PromptToolDefinition } from './tools.js';
+
+interface AIToolsSelectionOptions {
+  systemPrompt: string;
+  question: string;
+  functions: PromptToolDefinition[];
+}
+export async function aiToolsSelection(options: AIToolsSelectionOptions) {
+  const chatCompletion = await openaiClient.chat.completions.create({
+    model: 'gpt-4o-mini',
+    messages: [
+      {
+        role: 'system',
+        content: options.systemPrompt,
+      },
+      {
+        role: 'user',
+        content: options.question,
+      },
+    ],
+    tools: options.functions.map((func) => ({
+      type: 'function',
+      function: func,
+    })),
+    tool_choice: 'required',
+    stream: false,
+  });
+
+  const calls = chatCompletion.choices[0].message.tool_calls;
+
+  if (!calls) {
+    return null;
+  }
+
+  const selectedFunction = calls[0].function;
+
+  try {
+    return {
+      name: selectedFunction.name,
+      arguments: JSON.parse(selectedFunction.arguments) satisfies Record<
+        string,
+        any
+      >,
+    };
+  } catch {
+    return null;
+  }
+}
