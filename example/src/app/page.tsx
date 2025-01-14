d8e642be953d8cd02199d91d73d76308e49a5b9d
moonrailgun <moonrailgun@gmail.com>
2025-01-15 01:17:19 +0800

    refactor: example repo add send tianji report support

--- example/src/app/page.tsx
+++ example/src/app/page.tsx
@@ -1,101 +1,94 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import { useLocalStorageState } from 'ahooks';
+import { initTianjiTracker, reportEvent } from 'tianji-client-react';
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [backendUrl, setBackendUrl] = useLocalStorageState(
+    'tianji.backendUrl',
+    {
+      defaultValue: 'http://localhost:12345',
+    }
+  );
+  const [websiteId, setWebsiteId] = useLocalStorageState('tianji.websiteId', {
+    defaultValue: '',
+  });
+  const [injectedEl, setInjectedEl] = useState<HTMLScriptElement | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const injectTracker = async () => {
+    if (injectedEl) {
+      injectedEl.remove();
+      setInjectedEl(null);
+    }
+
+    if (!backendUrl || !websiteId) {
+      alert(
+        'Not has required field!' + JSON.stringify({ backendUrl, websiteId })
+      );
+      return;
+    }
+
+    const scriptEl = await initTianjiTracker({
+      url: backendUrl,
+      websiteId,
+    });
+
+    console.log('scriptEl', scriptEl);
+
+    setInjectedEl(scriptEl);
+  };
+
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-2 sm:items-start">
+        <div className="flex gap-2">
+          <div>
+            <div>Backend Url:</div>
+            <input
+              className="rounded-md border px-2 py-1"
+              value={backendUrl}
+              onChange={(e) => setBackendUrl(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+
+          <div>
+            <div>Website ID:</div>
+            <input
+              className="rounded-md border px-2 py-1"
+              value={websiteId}
+              onChange={(e) => setWebsiteId(e.target.value)}
+            />
+          </div>
         </div>
+
+        <button
+          className="w-full rounded-lg bg-gray-200 px-2 py-1 text-lg transition-all hover:bg-blue-500 hover:text-white"
+          onClick={injectTracker}
+        >
+          Inject
+        </button>
+
+        {injectedEl && (
+          <div>
+            <div>Injected!</div>
+            <div>websiteId: {injectedEl.getAttribute('data-website-id')}</div>
+
+            <div className="flex flex-col items-center gap-4 sm:flex-row">
+              <div
+                className="flex h-8 cursor-pointer items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
+                onClick={() => {
+                  reportEvent('Tianji Demo');
+
+                  console.log('report event to website');
+                }}
+              >
+                Send Tianji Event
+              </div>
+            </div>
+          </div>
+        )}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
