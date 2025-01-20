ea4b04cd6eab399a7ba4fe4264b94e8ef7338b5f
moonrailgun <moonrailgun@gmail.com>
2025-01-21 01:22:33 +0800

    chore: add openapi sdk options

--- packages/client-sdk/src/config.ts
+++ packages/client-sdk/src/config.ts
@@ -1,5 +1,12 @@
 import { OpenAPI } from './open/client';
 
-export function initOpenapiSDK(baseUrl: string) {
+interface OpenapiSDKOptions {
+  header?: Record<string, string>;
+}
+export function initOpenapiSDK(baseUrl: string, options?: OpenapiSDKOptions) {
   OpenAPI.BASE = baseUrl.endsWith('/open') ? baseUrl : `${baseUrl}/open`;
+
+  if (options?.header) {
+    OpenAPI.HEADERS = options.header;
+  }
 }
