ce7009248a97287d8b567f15465607ae68dc5cae
moonrailgun <moonrailgun@gmail.com>
2025-01-28 20:18:19 +0800

    chore: add more example playground events

--- example/src/app/page.tsx
+++ example/src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client';
-import Image from 'next/image';
 import { useLocalStorageState } from 'ahooks';
 import { initTianjiTracker, reportEvent } from 'tianji-client-react';
 import { useState } from 'react';
+import { toast } from 'sonner';
+import { Button, Code, TextField } from '@radix-ui/themes';
 
 export default function Home() {
   const [backendUrl, setBackendUrl] = useLocalStorageState(
@@ -41,50 +42,134 @@ export default function Home() {
 
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-2 sm:items-start">
-        <div className="flex gap-2">
-          <div>
+      <main className="row-start-2 flex w-[560px] flex-col items-center gap-2 sm:items-start">
+        <h1 className="mb-6 w-full text-center text-2xl font-semibold">
+          Tianji Event Playground
+        </h1>
+
+        <div className="flex w-full gap-2">
+          <div className="flex-1">
             <div>Backend Url:</div>
-            <input
-              className="rounded-md border px-2 py-1"
+
+            <TextField.Root
+              variant="surface"
+              placeholder="Input Backend Url"
               value={backendUrl}
               onChange={(e) => setBackendUrl(e.target.value)}
             />
           </div>
 
-          <div>
+          <div className="flex-1">
             <div>Website ID:</div>
-            <input
-              className="rounded-md border px-2 py-1"
+
+            <TextField.Root
+              variant="surface"
+              placeholder="Input Website ID"
               value={websiteId}
               onChange={(e) => setWebsiteId(e.target.value)}
             />
           </div>
         </div>
 
-        <button
-          className="w-full rounded-lg bg-gray-200 px-2 py-1 text-lg transition-all hover:bg-blue-500 hover:text-white"
+        <Button
+          className="!w-full !cursor-pointer"
+          radius="full"
           onClick={injectTracker}
         >
           Inject
-        </button>
+        </Button>
 
         {injectedEl && (
           <div>
             <div>Injected!</div>
-            <div>websiteId: {injectedEl.getAttribute('data-website-id')}</div>
+            <div className="mb-4">
+              websiteId:{' '}
+              <Code>{injectedEl.getAttribute('data-website-id')}</Code>
+            </div>
 
-            <div className="flex flex-col items-center gap-4 sm:flex-row">
-              <div
-                className="flex h-8 cursor-pointer items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
+            <div className="flex flex-col flex-wrap items-center gap-4 sm:flex-row">
+              <Button
+                className="!cursor-pointer"
+                variant="soft"
                 onClick={() => {
-                  reportEvent('Tianji Demo');
+                  reportEvent('Tianji Demo Event');
 
-                  console.log('report event to website');
+                  toast('report event to website');
                 }}
               >
                 Send Tianji Event
-              </div>
+              </Button>
+              <Button
+                className="!cursor-pointer"
+                variant="soft"
+                onClick={() => {
+                  reportEvent('Tianji String Event', {
+                    string: 'bar',
+                  });
+
+                  toast('report event to website');
+                }}
+              >
+                Send Tianji Event with String params
+              </Button>
+              <Button
+                className="!cursor-pointer"
+                variant="soft"
+                onClick={() => {
+                  reportEvent('Tianji Number Event', {
+                    number: Math.round(Math.random() * 1000),
+                  });
+
+                  toast('report event to website');
+                }}
+              >
+                Send Tianji Event with Number params
+              </Button>
+              <Button
+                className="!cursor-pointer"
+                variant="soft"
+                onClick={() => {
+                  reportEvent('Tianji Date Event', {
+                    date: new Date(),
+                  });
+
+                  toast('report event to website');
+                }}
+              >
+                Send Tianji Event with Date params
+              </Button>
+              <Button
+                className="!cursor-pointer"
+                variant="soft"
+                onClick={() => {
+                  reportEvent('Tianji Object Event', {
+                    object: {
+                      a: 1,
+                      b: '2',
+                      c: {
+                        d: 3,
+                      },
+                    },
+                  });
+
+                  toast('report event to website');
+                }}
+              >
+                Send Tianji Event with Object params
+              </Button>
+              <Button
+                className="!cursor-pointer"
+                variant="soft"
+                onClick={() => {
+                  reportEvent('Tianji Array Event', {
+                    array: [1, 2, 3, 4, 5],
+                  });
+
+                  toast('report event to website');
+                }}
+              >
+                Send Tianji Event with Array params
+              </Button>
             </div>
           </div>
         )}
