2880a2fc808c0ca475388d1587107768fb21a931
moonrailgun <moonrailgun@gmail.com>
2025-03-25 23:22:51 +0800

    test: add more test case for insight feature

--- src/server/model/insights/survey.spec.ts
+++ src/server/model/insights/survey.spec.ts
@@ -77,4 +77,260 @@ describe('SurveyInsightsSqlBuilder', () => {
     const sql = builder.build();
     expect(unwrapSQL(sql)).toMatchSnapshot('sql');
   });
+
+  test('with filters', () => {
+    const builder = new SurveyInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: '$all_event',
+            math: 'events',
+          },
+        ],
+        filters: [
+          {
+            name: 'country',
+            type: 'string',
+            operator: 'equals',
+            value: 'US',
+          },
+          {
+            name: 'age',
+            type: 'number',
+            operator: 'greater than',
+            value: 18,
+          },
+        ],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'day',
+        },
+        groups: [],
+      },
+      {
+        timezone: 'UTC',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with filters');
+  });
+
+  test('different metrics', () => {
+    const builder = new SurveyInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: 'rating',
+            math: 'events',
+          },
+          {
+            name: 'comment',
+            math: 'events',
+          },
+          {
+            name: '$all_event',
+            math: 'sessions',
+          },
+        ],
+        filters: [],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'day',
+        },
+        groups: [],
+      },
+      {
+        timezone: 'UTC',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with different metrics');
+  });
+
+  test('different time units', () => {
+    const builder = new SurveyInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: '$all_event',
+            math: 'events',
+          },
+        ],
+        filters: [],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'month',
+        },
+        groups: [],
+      },
+      {
+        timezone: 'Asia/Shanghai',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with month time unit');
+  });
+
+  test('combined filters and groups', () => {
+    const builder = new SurveyInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: '$all_event',
+            math: 'events',
+          },
+        ],
+        filters: [
+          {
+            name: 'completed',
+            type: 'boolean',
+            operator: 'equals',
+            value: 1,
+          },
+        ],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'day',
+        },
+        groups: [
+          {
+            value: 'userType',
+            type: 'string',
+          },
+        ],
+      },
+      {
+        timezone: 'UTC',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with filters and groups');
+  });
+
+  test('date type filters', () => {
+    const builder = new SurveyInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: '$all_event',
+            math: 'events',
+          },
+        ],
+        filters: [
+          {
+            name: 'submittedAt',
+            type: 'date',
+            operator: 'between',
+            value: [1739203200000, 1741881599999],
+          },
+        ],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'day',
+        },
+        groups: [],
+      },
+      {
+        timezone: 'UTC',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with date filters');
+  });
+
+  test('multiple string filters', () => {
+    const builder = new SurveyInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: '$all_event',
+            math: 'events',
+          },
+        ],
+        filters: [
+          {
+            name: 'source',
+            type: 'string',
+            operator: 'contains',
+            value: 'web',
+          },
+          {
+            name: 'feedback',
+            type: 'string',
+            operator: 'not contains',
+            value: 'bug',
+          },
+        ],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'day',
+        },
+        groups: [],
+      },
+      {
+        timezone: 'UTC',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with multiple string filters');
+  });
+
+  test('numeric filters with ranges', () => {
+    const builder = new SurveyInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: '$all_event',
+            math: 'events',
+          },
+        ],
+        filters: [
+          {
+            name: 'score',
+            type: 'number',
+            operator: 'between',
+            value: [7, 10],
+          },
+        ],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'day',
+        },
+        groups: [],
+      },
+      {
+        timezone: 'UTC',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with numeric range filter');
+  });
 });
--- src/server/model/insights/website.spec.ts
+++ src/server/model/insights/website.spec.ts
@@ -77,4 +77,184 @@ describe('WebsiteInsightsSqlBuilder', () => {
     const sql = builder.build();
     expect(unwrapSQL(sql)).toMatchSnapshot('sql');
   });
+
+  test('with filters', () => {
+    const builder = new WebsiteInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: '$all_event',
+            math: 'events',
+          },
+        ],
+        filters: [
+          {
+            name: 'country',
+            type: 'string',
+            operator: 'equals',
+            value: 'US',
+          },
+          {
+            name: 'age',
+            type: 'number',
+            operator: 'greater than',
+            value: 18,
+          },
+        ],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'day',
+        },
+        groups: [],
+      },
+      {
+        timezone: 'UTC',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with filters');
+  });
+
+  test('different metrics', () => {
+    const builder = new WebsiteInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: '$page_view',
+            math: 'events',
+          },
+          {
+            name: 'click',
+            math: 'events',
+          },
+          {
+            name: '$all_event',
+            math: 'sessions',
+          },
+        ],
+        filters: [],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'day',
+        },
+        groups: [],
+      },
+      {
+        timezone: 'UTC',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with different metrics');
+  });
+
+  test('different time units', () => {
+    const builder = new WebsiteInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: '$all_event',
+            math: 'events',
+          },
+        ],
+        filters: [],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'month',
+        },
+        groups: [],
+      },
+      {
+        timezone: 'Asia/Shanghai',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with month time unit');
+  });
+
+  test('combined filters and groups', () => {
+    const builder = new WebsiteInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: '$all_event',
+            math: 'events',
+          },
+        ],
+        filters: [
+          {
+            name: 'isNew',
+            type: 'boolean',
+            operator: 'equals',
+            value: 1,
+          },
+        ],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'day',
+        },
+        groups: [
+          {
+            value: 'browser',
+            type: 'string',
+          },
+        ],
+      },
+      {
+        timezone: 'UTC',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with filters and groups');
+  });
+
+  test('date type filters', () => {
+    const builder = new WebsiteInsightsSqlBuilder(
+      {
+        insightId,
+        insightType,
+        metrics: [
+          {
+            name: '$all_event',
+            math: 'events',
+          },
+        ],
+        filters: [
+          {
+            name: 'registeredAt',
+            type: 'date',
+            operator: 'between',
+            value: [1739203200000, 1741881599999],
+          },
+        ],
+        time: {
+          startAt: 1739203200000,
+          endAt: 1741881599999,
+          unit: 'day',
+        },
+        groups: [],
+      },
+      {
+        timezone: 'UTC',
+      }
+    );
+
+    const sql = builder.build();
+    expect(unwrapSQL(sql)).toMatchSnapshot('sql with date filters');
+  });
 });
