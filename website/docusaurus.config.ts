30834aad73179bd5f642833428dee4a68e4f3f11
moonrailgun <moonrailgun@gmail.com>
2025-06-27 23:13:50 +0800

    docs: change website domain to tianji.dev

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://tianji.msgbyte.com',
+  url: 'https://tianji.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
