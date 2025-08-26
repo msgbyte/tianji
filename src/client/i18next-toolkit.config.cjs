9cbd182be5eeaa0f4818846699bd9958c74dec83
Ali Güneş <82121296+aligunesv@users.noreply.github.com>
2025-08-26 10:44:34 +0300

    Add Turkish (tr) localization

--- src/client/i18next-toolkit.config.cjs
+++ src/client/i18next-toolkit.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('@i18next-toolkit/cli').I18nextToolkitConfig} */
 const config = {
-  locales: ['en', 'zh-CN', 'ja-JP', 'fr-FR', 'de-DE', 'pl-PL', 'pt-PT', 'ru-RU'],
+  locales: ['en', 'zh-CN', 'ja-JP', 'fr-FR', 'de-DE', 'pl-PL', 'pt-PT', 'ru-RU', 'tr-TR'],
   verbose: true,
   namespaces: ['translation'],
   translator: {
