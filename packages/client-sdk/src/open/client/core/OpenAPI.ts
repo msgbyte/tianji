8fa574c99670cc9f8393d2f832b87f06dd03e8a1
moonrailgun <moonrailgun@gmail.com>
2025-11-05 01:40:25 +0800

    chore: update translations and increment SDK version to 1.30.5

--- packages/client-sdk/src/open/client/core/OpenAPI.ts
+++ packages/client-sdk/src/open/client/core/OpenAPI.ts
@@ -47,7 +47,7 @@ export const OpenAPI: OpenAPIConfig = {
 	PASSWORD: undefined,
 	TOKEN: undefined,
 	USERNAME: undefined,
-	VERSION: '1.30.4',
+	VERSION: '1.30.5',
 	WITH_CREDENTIALS: false,
 	interceptors: {
 		request: new Interceptors(),
