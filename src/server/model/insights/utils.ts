45000edf7706bbb65d50b5e45aa0f25d418f461a
moonrailgun <moonrailgun@gmail.com>
2025-11-23 23:36:17 +0800

    refactor: move processGroupedTimeSeriesData utils to share code

--- src/server/model/insights/utils.ts
+++ src/server/model/insights/utils.ts
@@ -1,7 +1,4 @@
-import { z } from 'zod';
-import { insightsQuerySchema } from '../../utils/schema.js';
-import { get, uniq } from 'lodash-es';
-import { getDateArray } from '@tianji/shared';
+import { processGroupedTimeSeriesData } from '@tianji/shared';
 
 export const insightsSurveyBuiltinFields = [
   'aiCategory',
@@ -11,78 +8,9 @@ export const insightsSurveyBuiltinFields = [
   'language',
 ];
 
-/**
- * Process the grouped time series data
- * @param query - The query object
- * @param context - The context object
- * @param data - The database result
- * @returns The processed data
- */
-export function processGroupedTimeSeriesData(
-  query: z.infer<typeof insightsQuerySchema>,
-  context: { timezone: string },
-  data: { date: string | null }[]
-) {
-  const { time, metrics, groups } = query;
-  const { startAt, endAt, unit, timezone = context.timezone } = time;
-
-  let result: {
-    name: string;
-    [groupName: string]: any;
-    data: {
-      date: string;
-      value: number;
-    }[];
-  }[] = [];
-
-  for (const m of metrics) {
-    if (groups.length > 0) {
-      for (const g of groups) {
-        const allGroupValue = uniq(
-          data.map((item) => get(item, `%${g.value}`) as any)
-        );
-
-        result.push(
-          ...allGroupValue.map((gv) => ({
-            name: m.name,
-            alias: m.alias,
-            [g.value]: gv,
-            data: getDateArray(
-              data
-                .filter((item) => get(item, `%${g.value}`) === gv)
-                .map((item) => {
-                  return {
-                    value: Number(get(item, m.alias ?? m.name)),
-                    date: String(item.date),
-                  };
-                }),
-              startAt,
-              endAt,
-              unit,
-              timezone
-            ),
-          }))
-        );
-      }
-    } else {
-      result.push({
-        name: m.name,
-        alias: m.alias,
-        data: getDateArray(
-          data.map((item) => {
-            return {
-              value: Number(get(item, m.alias ?? m.name)),
-              date: String(item.date),
-            };
-          }),
-          startAt,
-          endAt,
-          unit,
-          timezone
-        ),
-      });
-    }
-  }
-
-  return result;
-}
+export {
+  /**
+   * @deprecated please direct import from `@tianji/shared`
+   */
+  processGroupedTimeSeriesData,
+};
--- src/shared/src/insights.ts
+++ src/shared/src/insights.ts
@@ -1,3 +1,6 @@
+import { get, uniq } from 'lodash-es';
+import { DateUnit, getDateArray } from './date';
+
 export interface MetricsInfo {
   name: string;
   math: 'events' | 'sessions' | 'p50' | 'p90' | 'p95' | 'p99' | 'avg';
@@ -51,3 +54,95 @@ export type FilterOperator =
   | FilterStringOperator
   | FilterBooleanOperator
   | FilterDateOperator;
+
+export type GroupedTimeSeriesQuery = {
+  time: {
+    startAt: number;
+    endAt: number;
+    unit: DateUnit;
+    timezone?: string;
+  };
+  metrics?: {
+    name: string;
+    alias?: string;
+  }[];
+  groups?: {
+    value: string;
+  }[];
+};
+
+/**
+ * Process the grouped time series data
+ * @param query - The query object
+ * @param context - The context object
+ * @param data - The database result
+ * @returns The processed data
+ */
+export function processGroupedTimeSeriesData(
+  query: GroupedTimeSeriesQuery,
+  context: { timezone: string },
+  data: { date: string | null }[]
+) {
+  const { time, metrics = [], groups = [] } = query;
+  const { startAt, endAt, unit, timezone = context.timezone } = time;
+
+  let result: {
+    name: string;
+    [groupName: string]: any;
+    data: {
+      date: string;
+      value: number;
+    }[];
+  }[] = [];
+
+  for (const m of metrics) {
+    if (groups.length > 0) {
+      for (const g of groups) {
+        const allGroupValue = uniq(
+          data.map((item) => get(item, `%${g.value}`) as any)
+        );
+
+        result.push(
+          ...allGroupValue.map((gv) => ({
+            name: m.name,
+            alias: m.alias,
+            [g.value]: gv,
+            data: getDateArray(
+              data
+                .filter((item) => get(item, `%${g.value}`) === gv)
+                .map((item) => {
+                  return {
+                    value: Number(get(item, m.alias ?? m.name)),
+                    date: String(item.date),
+                  };
+                }),
+              startAt,
+              endAt,
+              unit,
+              timezone
+            ),
+          }))
+        );
+      }
+    } else {
+      result.push({
+        name: m.name,
+        alias: m.alias,
+        data: getDateArray(
+          data.map((item) => {
+            return {
+              value: Number(get(item, m.alias ?? m.name)),
+              date: String(item.date),
+            };
+          }),
+          startAt,
+          endAt,
+          unit,
+          timezone
+        ),
+      });
+    }
+  }
+
+  return result;
+}
